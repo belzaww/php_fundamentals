96ec5798a6967d39dc738e176c491c19ba145df2
Vlad Belza <belzaww@gmail.com>
2024-12-23 14:46:55 +0200

    Fun Facts about php

---
+++ 02/funfacts.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="facts.css">
+    <title>Fun Facts About PHP</title>
+  
+</head>
+<body>
+    <?= "<h1>Fun Facts About PHP </h1>" ?>
+    <?php
+
+        echo "<ul>
+        <li class=\"fact1\">PHP stands for 'PHP: Hypertext Preprocessor'.</li>
+        <li class=\"fact2\">PHP is used by 78.9% of all websites with a known server-side programming language.</li>
+        <li class=\"fact3\">PHP was created in 1994 by Rasmus Lerdorf.</li>
+    </ul>";
+
+    ?>
+</body>
+</html>
