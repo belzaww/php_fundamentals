b413ec04d87179fb05b376078bc91d7ee7ebd812
Vlad Belza <belzaww@gmail.com>
2024-12-23 14:28:34 +0200

    goals html page

---
+++ 02/goals.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Future Goals</title>
+</head>
+<body>
+    <?= "<h1>List of my goals</h1>" ?>
+
+    <?php
+       echo "
+        <ol>
+            <li>Learn <strong>PHP</strong> and build amazing <span style=\" font-style: italic; \">websites</span>
+            <p>Just additional text!</p>
+            </li>
+            <li>Become a professional developer
+            <p>Just additional text!</p>
+            </li>
+            <li>Learn English
+            <p>Just additional text!</p>
+            </li>
+        </ol>";
+    ?>
+</body>
+</html>
