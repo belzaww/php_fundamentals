7e21c24f84f3c3819b057eca72d95e32b6165b31
Vlad Belza <belzaww@gmail.com>
2025-01-02 14:16:35 +0200

    Changing of variable

---
+++ 03/variablesexperiments.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables Experiments</title>
+</head>
+<body>
+    <?php
+
+        $number = 10;
+        $number += 5;
+        $number *= 2;
+        $number /= 10;
+
+        echo "The final value of number is $number.";
+
+    ?>
+</body>
+</html>
