dd7607da3a51bd43efe58e76101b92ef55b62f6b
Vlad Belza <belzaww@gmail.com>
2025-01-02 16:05:18 +0200

    test1

--- 03/combo.php
+++ 03/combo.php
@@ -7,19 +7,7 @@
     <title>Statistics</title>
 </head>
 <body>
-    <?php
-    
-        $discount = 40;
-        $quantity = 10;
-        $price = 5;
-        $total_price = $price * $quantity;
-        $discount_price = ($total_price / 100) * $discount;
-        $final_price = $total_price - $discount_price; 
-        $item = $quantity > 1 ? 'books' : 'book';
-       
-        
-        echo "<p style='color: blue; font-weight: bold;'>You bought $quantity $item for total price ".  $final_price ." dollars after a $discount% discount.</p>";
-
-    ?>
+    <p>Hello</p>
+  
 </body>
 </html>
