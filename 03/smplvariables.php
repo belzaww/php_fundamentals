c51c4d041740ea307796aaf2f1d2852b2974076c
Vlad Belza <belzaww@gmail.com>
2025-01-02 13:38:05 +0200

    Simple Variables

---
+++ 03/smplvariables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple variables</title>
+</head>
+<body>
+    <?php
+    
+        $name = 'Vlad';
+        $age = 20;
+        $hobby = 'chilling out';
+         
+        echo "<p>My name is $name. I am $age years old. My hobby is $hobby.</p>"
+    
+    ?>
+</body>
+</html>
