ada190d3f923bbd0d75fe06c797f821fea248244
Vlad Belza <belzaww@gmail.com>
2025-01-03 14:47:26 +0200

    Examples from lesson

---
+++ 04/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+define('PASSWORD','password');
+define('PASSWORD_2','password2');
+// define('PASSWORD','password3');
+
+
+// echo PASSWORD."<br>";
+// echo PASSWORD_2;
+
+const PASSWORD3 = 'password3';
+
+echo PASSWORD3;
+// echo PASSWORd3;
+
+
+
+
